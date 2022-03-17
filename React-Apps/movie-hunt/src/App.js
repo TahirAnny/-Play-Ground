bb8020b4369d95932d62baa6fb9e30750d87c8c8
TahirAnny <tahiraAnny@gmail.com>
2022-03-17 23:12:14 +0600

    Omdb api key added and call using fetch

--- React-Apps/movie-hunt/src/App.js
+++ React-Apps/movie-hunt/src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import {React, useEffect} from "react";
+
+const API_URL = 'http://www.omdbapi.com?apiKey=1a85dfc9';
 
 const App = () => {
+
+
+    const serachMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        serachMovies('spiderman');
+    }, []);
+
     return(
         <h1>Hello!!!</h1>
     );
