0b84a57453f014a5eec09330019bb52f21a3264e
TahirAnny <tahiraanny@gmail.com>
2022-03-27 18:35:01 +0600

    window binding

---
+++ JavaScript/this-key/4.window-binding.js
@@ -0,0 +1,20 @@
+/*
+    If the this keyword is not resolved with any of the bindings, implicit, explicit or new, 
+    then the this is bound to the window(global) object.
+*/
+
+let personName = function(name) {
+    console.log(this.name);
+};
+
+global.name = 'Soton'; //In a browser it is named window, for Node.js it is global
+personName();
+
+/*
+    NOTE: JavaScript strict mode does not allow this default binding.
+*/
+
+"use strict";
+function myFunction() {
+  return this;
+}
