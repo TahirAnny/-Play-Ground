42c559b7e3b5eeb15c18afebca99c4c1d9a6a6f3
TahirAnny <tahiraAnny@gmail.com>
2022-05-04 19:50:28 +0600

    create element example added

--- React-Apps/Exercise/src/index.js
+++ React-Apps/Exercise/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -15,3 +15,19 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// const element = React.createElement(
+//   'h1',
+//   {className: 'hello'},
+//   'Hello World'
+// )
+
+// console.log(element);
+
+//HTML element creation
+// const para = document.createElement("p");
+// para.innerText = "This is a paragraph";
+// document.body.appendChild(para);
+
+// console.dir(para)
