9595369583d6bc0cf0fdd401f327178a540bb253
TahirAnny <tahiraAnny@gmail.com>
2022-05-06 19:38:29 +0600

    spread operator

---
+++ JavaScript/4. Spread-Operator/spreadOperator.js
@@ -0,0 +1,9 @@
+//Spread operator -> Expand array or object
+//Rest Operator -> To get all the parameters or rest of the parameters
+
+var arr = [1, 2, 3];
+var arr1 = [...arr, 4, 5, 6];
+console.log(arr1);
+
+
+
