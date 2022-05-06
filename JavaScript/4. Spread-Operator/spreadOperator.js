4ebe896cb91bcee8320004483be4cfc4aabbef87
TahirAnny <tahiraAnny@gmail.com>
2022-05-06 19:41:42 +0600

    spread operator

--- JavaScript/4. Spread-Operator/spreadOperator.js
+++ JavaScript/4. Spread-Operator/spreadOperator.js
@@ -5,5 +5,22 @@ var arr = [1, 2, 3];
 var arr1 = [...arr, 4, 5, 6];
 console.log(arr1);
 
+//Apply for new operator
+
+let dateFields = [1970, 0, 1];  // 1 Jan 1970
+let d = new Date(...dateFields);
+
+console.log(d)
+
+// Function
+
+function sum(x, y, z) {
+    return x + y + z;
+  }
+
+const numbers = [1, 2, 3];
+console.log(sum(...numbers));
+ 
+
 
 
